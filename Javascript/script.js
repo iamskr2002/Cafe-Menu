2552b1a9c00c54e8bbad11d057cddd958b5c1af5
Subham Kumar Raya <118835736+iamskr2002@users.noreply.github.com>
2024-02-01 02:51:53 +0000

    puush

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,6 +1,17 @@
-function myName(nam){
-    console.log(nam);
-}
+let modeBtn = document.querySelector("#mode");
+let body = document.querySelector("body");
+let currMode = "light";
 
-let fullName = 'subham';
-myName(fullName);
+modeBtn.addEventListener("click", () => {
+    if ( currMode == "light"){
+        currMode = "dark";
+        body.classList.add("dark");
+        body.classList.remove("light");
+    } else{
+        currMode = "light";
+        body.classList.add("light");
+        body.classList.remove("dark");
+    }
+
+    console.log(currMode);
+});
